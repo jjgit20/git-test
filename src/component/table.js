b72ed0b4d67165a1a5061ae7d4bbe1a1454e3622
Jio Jang <talktalk0917@gmail.com>
2023-05-10 21:42:28 +0900

    changes

--- src/component/table.js
+++ src/component/table.js
@@ -7,54 +7,41 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
-  return (
-    <TableContainer component={Paper}>
+function jsonTable(props) {
+    
+    return (
+  
+      <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="left">사업명</TableCell>
+            <TableCell align="left">항목명</TableCell>
+            <TableCell align="left">예산안</TableCell>
+            <TableCell align="left">결산안</TableCell>
+            <TableCell align="left">집행율</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+      {props.json.map(item => (
+          <Fragment>
+              <TableRow>
+                  <TableCell rowSpan={item.항목.length + 1}>
+                  {item.사업명}
+                  </TableCell>
+              </TableRow>
+              {item.항목.map(subItem => (                                  
+                      <TableRow>
+                          <TableCell>{Object.keys(subItem)[0]}</TableCell>
+                          <TableCell>￦{subItem[Object.keys(subItem)[0]].예산안}</TableCell>
+                          <TableCell>￦{subItem[Object.keys(subItem)[0]].결산안}</TableCell>
+                          <TableCell>{subItem[Object.keys(subItem)[0]].집행률}%</TableCell>
+                      </TableRow>
+              ))}
+          </Fragment>
+      ))}
+  </TableBody>
       </Table>
     </TableContainer>
   );
-}
+  }
