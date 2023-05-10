fc6f841ca107228ab90b88cae0d954823e4caa1b
Jio Jang <talktalk0917@gmail.com>
2023-05-10 23:34:01 +0900

    sfks

--- src/App.js
+++ src/App.js
@@ -12,30 +12,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import './App.css';
-// import DataTable from './components/DataTable';
-// import jsondata from './testJson.json';
-
-// function App() {
-
-//   return (
-//       <div class="layout">
-//         <div class="logo">
-//           <img alt="logoimage" src='logo.png' style={{margin:10}}/>
-//         </div>
-//         <div class="container">
-//           <div class="menu">메뉴</div>
-//           <div class="container2">
-//             <div class="title">KAIST 학부총학생회 23년도 상반기 예결산안</div>
-//             <DataTable jsondata={jsondata}></DataTable> 
-//           </div>
-//         </div>
-//       </div>
-
-//   );
-// }
-
-// export default App;
